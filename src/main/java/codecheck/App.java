4510abd6ca133a3b6e4802dedc50fb651f2e1bbc
tie052312 <tie052312@S1611C11952-T1.tisnt.tis.co.jp>
2018-06-22 19:44:03 +0900

    commit

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -2,9 +2,27 @@ package codecheck;
 
 public class App {
 	public static void main(String[] args) {
-		for (int i = 0, l = args.length; i < l; i++) {
-			String output = String.format("argv[%s]: %s", i, args[i]);
-			System.out.println(output);
+
+		String[] param1 = args[0].split(":");
+		String[] param2 = args[1].split(":");
+		String str = args[2];
+
+		String result = null;
+
+
+		  if (Integer.parseInt(str) % Integer.parseInt(param1[0]) == 0) {
+
+			  result = param1[1];
+
+		  } else if (Integer.parseInt(str) % Integer.parseInt(param2[0]) == 0)  {
+
+			  result = result + param2[1];
+
+
+		  } else {
+			  result  = str;
+		  }
+
+			System.out.println(result);
 		}
-	}
 }
